50aead52fa24d8bb8ae17971ce04c4d8ead7110c
aliyun4911381062 <2879895063@qq.com>
2024-06-04 14:00:32 +0800

    add新手指南

---
+++ pages/greenhorn/greenhorn.vue
@@ -0,0 +1,40 @@
+<template>
+  <view class="new">
+      <view class="top1">
+      <p >退款什么时候到账</p> <p>></p>
+      </view>
+      <view class="top" v-for="item in list" :key="item.id">
+        <p>{{item.title1}}</p> <p>{{item.title2}}</p>
+      </view>
+  </view>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+  const list = ref([
+    {id:1,title1:"退换货运费谁承担",title2:'>'},
+    {id:2,title1:"如何查看我的付款记录",title2:'>'},
+    {id:3,title1:"下单后如何支付",title2:'>'},
+    {id:4,title1:"养车服务如何核销订单",title2:'>'},
+    ]
+  )
+  
+</script>
+
+<style lang="scss">
+.new{
+  width: 100%;
+  .top1{
+    padding: 5%;
+    border-top: 1rpx solid #fbfbfb;
+    display: flex;
+    justify-content: space-between;
+  }
+  .top{
+    padding: 5%;
+    border-top: 1rpx solid #fbfbfb;
+    display: flex;
+    justify-content: space-between;
+  }
+}
+</style>
