a8117470f8abd29ed080685dfa44e0c96b7bb9f1
mowuchenxiao <643348224@qq.com>
2024-06-04 11:32:04 +0800

    start

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 import App from './App'
-import uviewPlus from 'uview-plus'
+
 // #ifndef VUE3
 import Vue from 'vue'
-// main.js
-import uView from "uview-ui";
-
-Vue.use(uView);
 import './uni.promisify.adaptor'
 Vue.config.productionTip = false
 App.mpType = 'app'
@@ -16,13 +12,9 @@ app.$mount()
 // #endif
 
 // #ifdef VUE3
-import {
-  createSSRApp
-} from 'vue'
-
+import { createSSRApp } from 'vue'
 export function createApp() {
   const app = createSSRApp(App)
-  app.use(uviewPlus)
   return {
     app
   }
