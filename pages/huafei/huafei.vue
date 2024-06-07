ca6b2102aa0bde12412892fdeb523429b8f2d951
董树彬 <3104757657@qq.com>
2024-06-07 08:29:12 +0800

    add

--- pages/huafei/huafei.vue
+++ pages/huafei/huafei.vue
@@ -6,13 +6,15 @@
     <!-- 话费图片 -->
     <!-- 输入手机号 -->
     <view class="box2">
-      <uv-input placeholder="请输入你想要的内容" style="color: white;" v-model="value" @change="change"></uv-input>
+      <uv-input placeholder="请输入手机号" style="color: white;" v-model="value" @change="change"></uv-input>
     </view>
-    <view class="box3" v-for="(item, index) in list" :key="index">
-      <view class="box3-content">
-        <view>{{ item.text1 }}{{ item.text2 }}</view>
+    <view class="box3">
+      <view class="box3-content" v-for="(item, index) in list" :key="index">
+        <view class="box3-content-top">{{ item.text1 }}</view>
+        <view class="box3-content-bottom">{{ item.text2 }}</view>
       </view>
     </view>
+
   </view>
 
 </template>
@@ -41,6 +43,8 @@ export default {
 /* 话费充值截图 */
 .box {
   width: 100%;
+  height: 625px;
+  background-color: #F5F5F5;
 }
 
 .box1-img {
@@ -58,20 +62,33 @@ export default {
   width: 95%;
   margin: auto;
   margin-top: 15px;
+  background-color: #FFFFFF;
 }
 
 /* 充值余额 */
 .box3 {
-  width: 95%;
-  margin: auto;
+  width: 100%;
   display: flex;
+  flex-wrap: wrap;
+
 }
 
 .box3-content {
   width: 45%;
- 
+  margin: auto;
   margin-top: 20px;
+  background-color: #FFFFFF;  
+  text-align: center;
+  border-radius: 5px;
+  height: 60px;
 
-  border: 1px solid red;
+}
+.box3-content-top{
+  margin-top: 10px;
+}
+.box3-content-bottom{
+  color: red;
+  font-size: 10px;
+  
 }
 </style>
