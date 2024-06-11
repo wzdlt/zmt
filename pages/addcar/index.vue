6362efff4b47cdabba9d4d75ed24901cba5181a1
未知的路途 <1774954352@qq.com>
2024-06-11 09:08:43 +0800

    1

---
+++ pages/addcar/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <view class="header">
+      <view 
+      class="left"
+      @click="syy">
+          &lt;
+      </view>
+  
+      <view class="center">添加车辆</view>
+  </view>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+  .header {
+      position: fixed;
+      top: 0;
+      width: 100%;
+      height: 40px;
+      display: flex;
+      background-color: #fff;
+      border-bottom: 1px solid #ccc;
+      .left{
+          width: 40px;
+          height: 40px;
+          text-align: center;
+          line-height: 40px;
+      }
+      .center{
+          flex: 1;
+          text-align: center;
+          line-height: 40px;
+      }
+  }
+</style>
