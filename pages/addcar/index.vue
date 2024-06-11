8217d65c651027970a47860fb233d3e26edd93d3
董树彬 <3104757657@qq.com>
2024-06-11 09:32:22 +0800

    add

---
+++ pages/addcar/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <view class="header">
+      <view 
+      class="left"
+      @click="syy">
+          &lt;
+      </view>
+  
+      <view class="center">添加车辆</view>
+  </view>
+  <div class="richscan">
+    <div class="item">
+      <div class="i-left"></div>
+      <div class="i-input">
+        <input class="uni-input" type="number" placeholder="请输入车架号" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+  .header {
+      position: fixed;
+      top: 0;
+      width: 100%;
+      height: 40px;
+      display: flex;
+      background-color: #fff;
+      border-bottom: 1px solid #ccc;
+      .left{
+          width: 40px;
+          height: 40px;
+          text-align: center;
+          line-height: 40px;
+      }
+      .center{
+          flex: 1;
+          text-align: center;
+          line-height: 40px;
+      }
+  }
+</style>
--- pages/application/application.vue
+++ pages/application/application.vue
@@ -1,26 +1,96 @@
 <!-- 二手交易商申请 -->
 <template>
-  <view>
-
+  <view class="application">
+    <uv-form :model="form" :rules="rules" ref="form">
+      <uv-form-item label="姓名" prop="username">
+        <uv-input v-model="form.username" placeholder="请输入联系人姓名" />
+      </uv-form-item>
+      <uv-form-item label="电话" prop="mobile">
+        <uv-input v-model="form.mobile" placeholder="请输入联系电话" />
+      </uv-form-item>
+      <uv-form-item prop="pics">
+        <view class="text">照片</view>
+        <uv-upload :fileList="fileList" name="1" multiple :maxCount="9" @afterRead="afterRead" @delete="deletePic"
+          :previewFullImage="true"></uv-upload>
+      </uv-form-item>
+    </uv-form>
+    <uv-button @click="submit" text="提交" color="#f7433d"></uv-button>
   </view>
 </template>
-
 <script>
-  import {
-    Login
-  } from '@/api/index.js'
   export default {
     data() {
       return {
-
-      }
+        form: {
+          username: '',
+          mobile: '',
+          pics: ''
+        },
+        rules: {
+          username: [{
+            required: true,
+            message: '此为必填字段',
+            trigger: ['blur', 'change']
+          }],
+          pics: {
+            type: 'array',
+            required: true,
+            message: '请上传照片',
+            trigger: ['blur', 'change']
+          },
+          mobile: [{
+            required: true,
+            message: '此为必填字段',
+            // blur和change事件触发检验
+            trigger: ['blur', 'change']
+          }, {
+            // 自定义验证函数，见上说明，注意这里面的逻辑不能出现语法报错，否则可能导致不验证
+            validator: (rule, value, callback) => {
+              // 此处业务逻辑省略
+              // 返回true表校验通过，返回false表示不通过
+              return true;
+            },
+            message: '电话号码格式错误',
+            trigger: ['blur']
+          }]
+        }
+      };
+    },
+    onReady() {
+      this.$refs.form.setRules(this.rules);
     },
     methods: {
+      submit() {
+        this.$refs.form.validate().then(res => {
+          uni.showToast({
+            icon: 'success',
+            title: '校验通过'
+          })
+        }).catch(errors => {
+          uni.showToast({
+            icon: 'error',
+            title: '校验失败'
+          })
+        })
+      }
+    },
+  };
+</script>
+<style lang="scss">
+  .application {
+    padding: 30rpx;
 
+    ::v-deep .uv-form-item__body__right__content__slot {
+      display: block;
+
+      .text {
+        margin-bottom: 30rpx;
+      }
+    }
+
+    .uv-button--square {
+      margin: 30rpx 0;
+      border-radius: 20px;
     }
   }
-</script>
-
-<style>
-
 </style>
