48a90dc4afbac2d75229ff9840cfd3c6a20be4a1
aliyun4911381062 <2879895063@qq.com>
2024-06-11 11:31:15 +0800

    add

---
+++ pages/details/details.vue
@@ -0,0 +1,54 @@
+<template>
+  <view class="details">
+    <view class="top" v-for="item, in list" :key="item">
+      <view class="top-left">
+        <view class="one">{{item.name}}</view>
+        <view class="two">{{item.data}}</view>
+      </view>
+      <view class="top-right" v-if="item.price>0 ? '#08b30a':'#fc4424'">{{item.price}}</view>
+    </view>
+  </view>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+  const list = ref([
+    {name:'消费', data:'2021/09/01 15:11:34',price:"-100"},
+    {name:'充值', data:'2021/09/01 15:11:34',price:"+100"},
+    {name:'会员分佣', data:'2021/09/01 15:11:34',price:"+100"},
+    {name:'代理商分佣', data:'2021/09/01 15:11:34',price:"+100"}
+  ])
+  console.log(list.value);
+</script>
+
+<style lang="less" scoped>
+page{
+  background-color: #f5f5f5;
+}
+.details{
+  width: 100%;
+  
+  .top{
+    padding: 3%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #ffffff;
+    border-bottom: 1px solid #f8f8f8;
+    .top-left{
+      width: 50%;
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+      .one{
+        width: 100%;
+      }
+      .two{
+        font-size: 0.8rem;
+        width: 100%;
+        color: #a5a5a5;
+      }
+    }
+  }
+}
+</style>
--- pages/invite/invite.vue
+++ pages/invite/invite.vue
@@ -1,26 +1,109 @@
 <template>
   <view class="invite">
-    <uv-tabs :list="list" @click="click"></uv-tabs>
+    <!-- 头 -->
+    <uv-tabs :list="list" lineColor="#fc4424" :scrollable="false" activeStyle="color:#fc4424"
+      inactiveStyle="color:#a8a8a8"></uv-tabs>
+    <!--  <view class="top" ref="a1">
+      <view class="one" @click="getColor()">直推</view>
+      <view class="one" @click="getColor()">间推</view>
+    </view> -->
+    <!-- 主题 -->
+    <view class="body">
+      <view class="all" v-for="item in userName" :key="item">
+        <view class="body-left">
+          <uv-image shape="circle" :src="item.img" width="40px" height="40px"></uv-image>
+          <view class="user">{{item.name}}</view>
+        </view>
+        <view class="body-right">{{item.data}}</view>
+      </view>
+    </view>
   </view>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      list: [{
-        name: '直推',
-      }, {
-        name: '间推',
-      },]
+<script setup>
+  import {
+    ref
+  } from 'vue';
+  // const a1 = ref(null)
+  const list = ref([{
+    name: '直推'
+  }, {
+    name: '间推'
+  }])
+  const userName = ref([{
+      img: "https://cdn.uviewui.com/uview/album/1.jpg",
+      name: '王天琦',
+      data: '2022-03-31'
+    },
+    {
+      img: "https://cdn.uviewui.com/uview/album/1.jpg",
+      name: '郑新',
+      data: '2022-03-31'
+    },
+    {
+      img: "https://cdn.uviewui.com/uview/album/1.jpg",
+      name: '周校长',
+      data: '2022-03-31'
+    },
+    {
+      img: "https://cdn.uviewui.com/uview/album/1.jpg",
+      name: '李坊',
+      data: '2022-03-31'
+    },
+    {
+      img: "https://cdn.uviewui.com/uview/album/1.jpg",
+      name: '李嘉诚',
+      data: '2022-03-31'
+    },
+    {
+      img: "https://cdn.uviewui.com/uview/album/1.jpg",
+      name: '李林颖',
+      data: '2022-03-31'
     }
-  },
-  methods: {
-    click(item) {
-      console.log('item', item);
-    }
-  }
-}
+  ])
+  // function getColor() {
+  //   console.log(a1.value);
+  //   // document.getElementsByClassName("one").style.color='#fc4424'
+  // }
 </script>
 <style lang="less" scoped>
-page {}
+  page {
+    background-color: #f5f5f5;
+  }
+
+  .invite {
+    width: 100%;
+
+    // 头
+    .uv-tabs {
+      background-color: #ffffff;
+    }
+
+    // 主题
+    .body {
+      width: 100%;
+
+      .all {
+        padding: 5%;
+        background-color: #ffffff;
+        display: flex;
+        align-items: center;
+        border-bottom: 1px solid #f8f8f8;
+        justify-content: space-between;
+
+        .body-left {
+          width: 30%;
+          display: flex;
+          align-items: center;
+          .user {
+            margin-left: 3%;
+          }
+        }
+
+        .body-right {
+          color: #808080;
+        }
+      }
+
+    }
+  }
 </style>
