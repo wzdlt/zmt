98d803410fd78c9125329f874033643182449886
则安 <3324093317@qq.com>
2024-06-12 08:07:44 +0800

    0612-二手商品详情

---
+++ pages/twocommodity/twocommodity.vue
@@ -0,0 +1,23 @@
+<!-- 二手商品详情 -->
+<template>
+  <view>
+    
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style>
+
+</style>
