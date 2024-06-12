eb91def5ec7fac532e5e490561dea4094859289d
aliyun4911381062 <2879895063@qq.com>
2024-06-12 14:40:47 +0800

    add

--- pages/car/car.vue
+++ pages/car/car.vue
@@ -1,11 +1,34 @@
 <template>
-  <view>
-    这是养车页面
+  <view class="car">
+    <view class="top">
+      <view class="top-all">
+        <uv-icon name="arrow-left" color="#ffffff" size="20"></uv-icon>
+        <view>正茂通养车</view>
+      </view>
+    </view>
   </view>
 </template>
 
-<script setup>
-
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    onLoad() {},
+    methods: {}
+  }
 </script>
-
-<style></style>
+<style lang="less" scoped>
+  .car {
+    width: 100%;
+    .top {
+      padding-top: 44px;
+      width: 100%;
+      height: 300px;
+      background: linear-gradient(#fc4424, #f6e5e2);
+      .all{
+        border: 1px solid black;
+      }
+    }
+  }
+</style>
