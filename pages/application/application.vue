59a7aeca44a430cbda2dc4bbd2af86d5248b137e
则安 <3324093317@qq.com>
2024-06-12 08:05:02 +0800

    0612

--- pages/application/application.vue
+++ pages/application/application.vue
@@ -1,172 +1,376 @@
 <!-- 二手交易商申请 -->
 <template>
-  <view class="application">
-    <uv-form :model="form" :rules="rules" ref="form">
-      <uv-form-item label="名称" prop="firm">
-        <uv-input v-model="form.firm" placeholder="请输入公司名称" />
-      </uv-form-item>
-      <uv-form-item label="姓名" prop="username">
-        <uv-input v-model="form.username" placeholder="请输入联系人姓名" />
-      </uv-form-item>
-      <uv-form-item label="身份证号" prop="card">
-        <uv-input v-model="form.card" placeholder="请输入身份证号" />
-      </uv-form-item>
-      <uv-form-item label="电话" prop="mobile">
-        <uv-input v-model="form.mobile" placeholder="请输入联系电话" />
-      </uv-form-item>
-      <uv-form-item label="验证码" prop="captcha">
-        <uv-input v-model="form.captcha" placeholder="请输入验证码" />
-      </uv-form-item>
-      <uv-form-item label="地址" prop="address">
-        <uv-input v-model="form.addresss" placeholder="请输入门店地址" />
-      </uv-form-item>
-      <uv-form-item prop="pics">
-        <view class="text">照片</view>
-        <uv-upload v-model="form.pics" :fileList="fileList" name="1" multiple :maxCount="2" @afterRead="afterRead"
-          @delete="deletePic" :previewFullImage="true"></uv-upload>
-      </uv-form-item>
-      <uv-form-item>
-        <view class="text">商家资质证件</view>
-        <uv-upload v-model="form.pics" :fileList="fileList" name="1" multiple :maxCount="2" @afterRead="afterRead"
-          @delete="deletePic" :previewFullImage="true"></uv-upload>
-      </uv-form-item>
-      <uv-form-item prop="pics">
-        <view class="text">商家门店图片</view>
-        <uv-upload v-model="form.pics" :fileList="fileList" name="1" multiple :maxCount="2" @afterRead="afterRead"
-          @delete="deletePic" :previewFullImage="true"></uv-upload>
-      </uv-form-item>
-      <uv-form-item prop="introduce">
-        <view class="text">商家介绍</view>
-        <uv-textarea v-model="form.introduce" placeholder="请输入商家介绍"></uv-textarea>
-      </uv-form-item>
-      <uv-radio-group>
-        <uv-radio shape="square" name="112">同意二手交易商入驻协议</uv-radio>
-      </uv-radio-group>
-    </uv-form>
-    <uv-button @click="submit" text="提交" color="#f7433d"></uv-button>
+  <view class="box">
+    <view class="box1">
+      <view class="box1-1">
+        <view><text style="color: #fc492a;margin-right: 5px;">*</text>代理名称</view>
+        <view><input type="text" placeholder="请输入公司名称" /></view>
+      </view>
+      <view class="box1-2">
+        <view class="box1-2-left"><text style="color: #fc492a;margin-right: 5px;">*</text>logo</view>
+        <view class="box1-2-right">
+          <image src="../../static/image/addimage.png" v-if="logoimage" @click="huoqu"></image>
+          <view class="box1-2-right1" v-for="(image, index) in logoimage">
+            <image :src="image" @click="bigImg(image)"></image>
+            <view class="box1-2-right2" @click="bindImg(index)">
+              <image src="../../static/image/delete.png" mode=""></image>
+            </view>
+          </view>
+        </view>
+      </view>
+      <view class="box1-1">
+        <view><text style="color: #fc492a;margin-right: 5px;">*</text>联系人姓名</view>
+        <view><input type="text" placeholder="请填写联系人姓名" /></view>
+      </view>
+      <view class="box1-1">
+        <view><text style="color: #fc492a;margin-right: 5px;">*</text>身份证号</view>
+        <view><input type="idcard" placeholder="请填写身份证号" /></view>
+      </view>
+      <view class="box1-1">
+        <view><text style="color: #fc492a;margin-right: 5px;">*</text>联系电话</view>
+        <view><input type="number" placeholder="请填写联系电话" /></view>
+      </view>
+      <view class="box1-1">
+        <view><text style="color: #fc492a;margin-right: 5px;">*</text>验证码</view>
+        <view style="display: flex;">
+          <input type="number" placeholder="请填入验证码" />
+          <text style="color: #fc492a;margin-left: 10px;">获取验证码</text>
+        </view>
+      </view>
+      <view class="box1-1">
+        <view><text style="color: #fc492a;margin-right: 5px;">*</text>商家地址</view>
+        <view><input type="text" placeholder="请填写门店地址" /></view>
+      </view>
+
+      <view class="box1-5">
+        <view class="box1-2-left"><text style="color: #fc492a;margin-right: 5px;">*</text>上传身份证</view>
+        <view class="box1-2-right">
+          <view class="box1-2-right-1">
+            <image src="../../static/image/addimage.png" v-if="logoimage" @click="huoqu1"></image>
+          </view>
+          <view class="box1-2-right-1">
+            <image src="../../static/image/addimage.png" v-if="logoimage" @click="huoqu1"></image>
+          </view>
+          <view class="box1-2-right1" v-for="(image, index) in logoimage1">
+            <image :src="image" @click="bigImg(image)"></image>
+            <view class="box1-2-right2" @click="bindImg(index)">
+              <image src="../../static/image/delete.png" mode=""></image>
+            </view>
+          </view>
+        </view>
+      </view>
+      <view class="box1-5">
+        <view class="box1-2-left"><text style="color: #fc492a;margin-right: 5px;">*</text>商家资质证件(支持多张图片上传)</view>
+        <view class="box1-2-right">
+          <view class="box1-2-right-1">
+            <image src="../../static/image/addimage.png" v-if="logoimage" @click="huoqu2"></image>
+          </view>
+          <view class="box1-2-right1" v-for="(image, index) in logoimage2">
+            <image :src="image" @click="bigImg(image)"></image>
+            <view class="box1-2-right2" @click="bindImg(index)">
+              <image src="../../static/image/delete.png" mode=""></image>
+            </view>
+          </view>
+        </view>
+      </view>
+      <view class="box1-5">
+        <view class="box1-2-left"><text style="color: #fc492a;margin-right: 5px;">*</text>商家门店图片</view>
+        <view class="box1-2-right">
+          <view class="box1-2-right-1">
+            <image src="../../static/image/addimage.png" v-if="logoimage" @click="huoqu3"></image>
+          </view>
+          <view class="box1-2-right1" v-for="(image, index) in logoimage3">
+            <image :src="image" @click="bigImg(image)"></image>
+            <view class="box1-2-right2" @click="bindImg(index)">
+              <image src="../../static/image/delete.png" mode=""></image>
+            </view>
+          </view>
+        </view>
+      </view>
+
+      <view class="box1-3">
+        <view><text style="color: #fc492a;margin-right: 5px;">*</text>商家介绍</view>
+        <view><textarea @blur="bindTextAreaBlur" placeholder="请填写商家介绍" auto-height /></view>
+      </view>
+      <view class="box1-4">
+        <view class="box1-4-1">
+          <checkbox-group>
+            <label>
+              <checkbox value="cb" />
+            </label>
+          </checkbox-group>
+          同意代理<text style="color: #fc4424  ;">入驻协议</text>
+        </view>
+      </view>
+    </view>
+    <view class="box2">
+      <button>提交申请</button>
+    </view>
   </view>
 </template>
+
 <script>
   export default {
     data() {
       return {
-        form: {
-          firm: '',
-          username: '',
-          mobile: '',
-          pics: '',
-          card: '',
-          address: '',
-          captcha: '',
-          introduce: ''
-        },
-        rules: {
-          // 公司名称
-          firm: {
-            required: true,
-            message: '请输入公司名称',
-            trigger: ['blur', 'change']
-          },
-          // 联系人姓名
-          username: [{
-            required: true,
-            message: '请输入姓名',
-            trigger: ['blur', 'change']
-          }],
-          // 身份证号
-          card: {
-            required: true,
-            message: '请输入身份证号',
-            trigger: ['blur', 'change']
-          },
-          // 验证码
-          captcha: {
-            required: true,
-            message: '请输入验证码',
-            trigger: ['blur', 'change']
-          },
-          // 商家地址
-          address: {
-            required: true,
-            message: '请输入门店地址',
-            trigger: ['blur', 'change']
-          },
-          // 身份证
-          pics: {
-            type: 'array',
-            required: true,
-            message: '请上传照片',
-            trigger: ['blur', 'change']
-          },
-          // 电话
-          mobile: [{
-            required: true,
-            message: '请输入电话',
-            // blur和change事件触发检验
-            trigger: ['blur', 'change']
-          }, {
-            // 自定义验证函数，见上说明，注意这里面的逻辑不能出现语法报错，否则可能导致不验证
-            validator: (rule, value, callback) => {
-              // 此处业务逻辑省略
-              // 返回true表校验通过，返回false表示不通过
-              return true;
-            },
-            message: '电话号码格式错误',
-            trigger: ['blur']
-          }],
-          // 商家介绍
-          introduce: {
-            required: true,
-            message: '请输入商家介绍',
-            // blur和change事件触发检验
-            trigger: ['blur', 'change']
-          }
-        }
+        logoimage: [], // 存储选择的图片路径
+        logoimage1: [], // 存储选择的图片路径
+        logoimage2: [], // 存储选择的图片路径
+        logoimage3: [] // 存储选择的图片路径
       };
     },
-    onReady() {
-      this.$refs.form.setRules(this.rules);
-    },
     methods: {
-      submit() {
-        this.$refs.form.validate().then(res => {
-          uni.showToast({
-            icon: 'success',
-            title: '校验通过'
-          })
-        }).catch(errors => {
-          uni.showToast({
-            icon: 'error',
-            title: '校验失败'
-          })
-        })
+      huoqu() {
+        let that = this
+        uni.chooseImage({
+          count: 1, // 图片数量，这里设置为6
+          sizeType: ['original', 'compressed'], // 指定选择的图片类型，可以选择原图和压缩图，这里设置为二者都有
+          sourceType: ['album', 'camera'], // 指定选择图片的来源，这里设置为相册和相机
+          success: (res) => {
+            console.log(res.tempFilePaths)
+            that.logoimage.push(res.tempFilePaths[0]);
+          },
+          fail: (err) => {
+            console.log('选择图片失败', err);
+          }
+        });
+      },
+      bindImg(index) {
+        this.logoimage.splice(index, 1)
+      },
+      bigImg(url) {
+        uni.previewImage({
+          urls: this.logoimage,
+        });
+      },
+      huoqu1() {
+        let that = this
+        uni.chooseImage({
+          count: 1, // 图片数量，这里设置为6
+          sizeType: ['original', 'compressed'], // 指定选择的图片类型，可以选择原图和压缩图，这里设置为二者都有
+          sourceType: ['album', 'camera'], // 指定选择图片的来源，这里设置为相册和相机
+          success: (res) => {
+            console.log(res.tempFilePaths)
+            that.logoimage1.push(res.tempFilePaths[0]);
+          },
+          fail: (err) => {
+            console.log('选择图片失败', err);
+          }
+        });
+      },
+      bindImg(index) {
+        this.logoimage1.splice(index, 1)
+      },
+      bigImg(url) {
+        uni.previewImage({
+          urls: this.logoimage1,
+        });
+      },
+      huoqu2() {
+        let that = this
+        uni.chooseImage({
+          count: 1, // 图片数量，这里设置为6
+          sizeType: ['original', 'compressed'], // 指定选择的图片类型，可以选择原图和压缩图，这里设置为二者都有
+          sourceType: ['album', 'camera'], // 指定选择图片的来源，这里设置为相册和相机
+          success: (res) => {
+            console.log(res.tempFilePaths)
+            that.logoimage2.push(res.tempFilePaths[0]);
+          },
+          fail: (err) => {
+            console.log('选择图片失败', err);
+          }
+        });
+      },
+      bindImg(index) {
+        this.logoimage2.splice(index, 1)
+      },
+      bigImg(url) {
+        uni.previewImage({
+          urls: this.logoimage2,
+        });
+      },
+      huoqu3() {
+        let that = this
+        uni.chooseImage({
+          count: 1, // 图片数量，这里设置为6
+          sizeType: ['original', 'compressed'], // 指定选择的图片类型，可以选择原图和压缩图，这里设置为二者都有
+          sourceType: ['album', 'camera'], // 指定选择图片的来源，这里设置为相册和相机
+          success: (res) => {
+            console.log(res.tempFilePaths)
+            that.logoimage3.push(res.tempFilePaths[0]);
+          },
+          fail: (err) => {
+            console.log('选择图片失败', err);
+          }
+        });
+      },
+      bindImg(index) {
+        this.logoimage3.splice(index, 1)
+      },
+      bigImg(url) {
+        uni.previewImage({
+          urls: this.logoimage3,
+        });
       }
-    },
-  };
+    }
+  }
 </script>
+
 <style lang="scss">
-  .application {
-    padding: 30rpx;
+  .box {
+    background-color: #ccc;
+    height: 100vh;
 
-    ::v-deep .uv-form-item__body__left__content__label {
-      font-size: 26rpx;
-    }
+    .box1 {
+      background-color: #fff;
 
-    ::v-deep .uv-form-item__body__right__content__slot {
-      display: block;
+      .box1-1 {
+        display: flex;
+        justify-content: space-between;
+        border-bottom: 1px solid #ccc;
+        padding: 15px 10px;
 
-      .text {
-        margin-bottom: 30rpx;
+        input {
+          text-align: right;
+        }
+      }
+
+      .box1-2 {
+        display: flex;
+        justify-content: space-between;
+        border-bottom: 1px solid #ccc;
+        padding: 15px 10px;
+        line-height: 60px;
+
+        .box1-2-right {
+          width: 80%;
+          display: flex;
+          justify-content: right;
+
+          image {
+            width: 60px;
+            height: 60px;
+
+          }
+
+          .box1-2-right1 {
+            width: 60px;
+            height: 60px;
+            position: relative;
+
+            image {
+              width: 60px;
+              height: 60px;
+            }
+
+            .box1-2-right2 {
+              position: absolute;
+              top: -14px;
+              right: 0;
+
+              image {
+                width: 20px;
+                height: 20px;
+              }
+            }
+          }
+        }
+      }
+
+      .box1-3 {
+        // display: flex;
+        // justify-content: space-between;
+        border-bottom: 1px solid #ccc;
+        padding: 15px 10px;
+
+        textarea {
+          width: 100%;
+          padding: 10px;
+          box-sizing: border-box;
+          border-radius: 10px;
+          border: 1px solid #ccc;
+          margin-top: 15px;
+        }
+      }
+
+      .box1-4 {
+        // display: flex;
+        // justify-content: space-between;
+        border-bottom: 1px solid #ccc;
+        padding: 15px 10px;
+        color: #9c9c9c;
+
+        .box1-4-1 {
+          display: flex;
+        }
+      }
+
+      .box1-5 {
+        // display: flex;
+        // justify-content: space-between;
+        border-bottom: 1px solid #ccc;
+        padding: 15px 10px;
+
+        .box1-2-left {
+          margin-bottom: 10px;
+        }
+
+        .box1-2-right {
+          width: 100%;
+          display: flex;
+          justify-content: space-between;
+
+          .box1-2-right-1 {
+            width: 47%;
+            height: 100px;
+            border-radius: 10px;
+            border: 2px solid #fc4424;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            image {
+              width: 30px;
+              height: 30px;
+
+            }
+          }
+
+          .box1-2-right1 {
+            width: 60px;
+            height: 60px;
+            position: relative;
+
+            image {
+              width: 60px;
+              height: 60px;
+            }
+
+            .box1-2-right2 {
+              position: absolute;
+              top: 0;
+              right: 0;
+
+              image {
+                width: 20px;
+                height: 20px;
+              }
+            }
+          }
+        }
       }
     }
 
-    .uv-radio {
-      margin: 30rpx 0;
-    }
+    .box2 {
+      width: 95%;
+      margin: auto;
+      padding: 30px 0 0;
 
-    .uv-button--square {
-      margin: 30rpx 0;
-      border-radius: 20px;
+      button {
+        color: #fff;
+        background-color: #fc4424;
+        border-radius: 25px;
+      }
     }
   }
 </style>
