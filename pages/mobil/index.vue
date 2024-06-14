b927eca9e3ee76424d1c940cc56dabd18e910840
未知的路途 <1774954352@qq.com>
2024-06-14 14:53:18 +0800

    1

--- pages/mobil/index.vue
+++ pages/mobil/index.vue
@@ -16,18 +16,28 @@
 </script>
 
 <style lang="scss">
+  page{
+    background-color: #f5f5f5;
+  }
   .header {
       position: fixed;
       top: 0;
       width: 100%;
-      height: 40px;
+      height: 50px;
       display: flex;
       z-index: 10;
       .left{
           width: 40px;
-          height: 40px;
+          height: 50px;
           text-align: center;
-          line-height: 40px;
+          line-height: 50px;
       }
+  }
+  
+  .banner{
+    width: 100%;
+    img{
+      width: 100%;
+    }
   }
 </style>
