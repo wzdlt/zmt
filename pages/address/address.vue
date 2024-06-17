4c7b2d37bc260a348193d9f2abf448462f7b3402
aliyun4911381062 <2879895063@qq.com>
2024-06-17 14:44:02 +0800

    完结

---
+++ pages/address/address.vue
@@ -0,0 +1,48 @@
+<template>
+	<view class="delivery">
+		<view class="box" v-for="item in list" :key="item.id">
+			<view>{{item.text}}</view>
+			<view class="number">{{item.title}}</view>
+		</view>
+		<view class="btn"><button>确定</button></view>
+	</view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const list = ref([
+	{id:1,text:'收货人',title:'请输入手机号'},
+	{id:2,text:'联系电话',title:'请输入姓名'},
+	{id:3,text:'所在地区',title:'请选择省市区'},
+	{id:4,text:'详细地址',title:'请填写详细地址'},
+	])
+</script>
+
+<style lang="scss" scoped>
+	page {
+		background-color: #f5f5f5;
+	}
+	.delivery{
+		width: 100%;
+		.box{
+			border-top: 1px solid #828282;
+			display: flex;
+			justify-content: space-between;
+			background-color: #ffffff;
+			padding: 5%;
+			.number{
+				color: #828282;
+			}
+		}
+		.btn{
+			width: 90%;
+			margin: auto;
+			margin-top: 5%;
+			button{
+				border-radius: 25px;
+				background-color: #fc4424;
+				color: #ffffff;
+			}
+		}
+	}
+</style>
---
+++ pages/pay/pay.vue
@@ -0,0 +1,62 @@
+<template>
+  <view class="pay">
+    <view class="img">
+      <image src="/static/image/支付成功.png" mode=""></image>
+    </view>
+    <view class="success">恭喜，支付成功</view>
+    <view class="btn">
+      <button>查看订单</button>
+      <button>返回首页</button>
+    </view>
+  </view>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+  .pay {
+    margin: auto;
+    width: 90%;
+
+    .img {
+      margin-top: 5%;
+      width: 80%;
+      height: 250px;
+      margin: auto;
+
+      image {
+        width: 100%;
+      }
+    }
+
+    .success {
+      color: #828282;
+      margin-top: 5%;
+      text-align: center;
+      font-size: 1.2rem;
+    }
+
+    // 按钮
+    .btn {
+      margin-top: 10%;
+      display: flex;
+      justify-content: space-between;
+
+      button:nth-child(1) {
+        border: 1px solid #f5f5f5;
+        border-radius: 10px;
+        width: 45%;
+        background-color: #ffffff;
+      }
+
+      button:nth-child(2) {
+        border: 1px solid #f5f5f5;
+        border-radius: 10px;
+        background-color: #ececec;
+        width: 45%;
+      }
+    }
+  }
+</style>
