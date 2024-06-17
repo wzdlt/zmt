34e37af52f43cef373513a2db00480a60632df6f
董树彬 <3104757657@qq.com>
2024-06-17 08:46:57 +0800

    add

--- pages/uv-uizujain/uv-uizujain.vue
+++ pages/uv-uizujain/uv-uizujain.vue
@@ -1,6 +1,6 @@
 <template>
   <view class="box">
-    <uv-popup ref="popup" mode="bottom" >
+    <uv-popup ref="popup" mode="bottom">
       <view class="content">
         <!-- 头部 -->
         <view class="content-top">
@@ -18,32 +18,55 @@
         </view>
         <!-- 头部结束 -->
         <view class="content-content">
-          <view>颜色</view>
-          <view>颜色</view>
-          <view>颜色</view>
-          <view>颜色</view>
-          <view>颜色</view>
+          <view style="margin-top: 20px;">颜色</view>
+
           <view class="content-button1">
             <view class=" content-button">
-              <uv-button type="primary" :plain="true" text="标准白"></uv-button>
-            </view>
-            <view class=" content-button">
-              <uv-button type="primary" :plain="true" text="经典黑"></uv-button>
-            </view>
-            <view class=" content-button">
-              <uv-button type="primary" :plain="true" text="纯净蓝"></uv-button>
-            </view>
-            <view class=" content-button">
-              <uv-button type="primary" :plain="true" text="中国红"></uv-button>
-            </view>
-            <view class=" content-button">
-              <uv-button type="primary" :plain="true" text="象牙白/浮云黑"></uv-button>
-            </view>
-            <view class=" content-button">
-              <uv-button type="primary" :plain="true" text="象牙白/复古绿"></uv-button>
+              <view class="content-2">
+                <uv-button type="primary" :plain="true" text="标准白"></uv-button>
+                <uv-button type="primary" :plain="true" text="经典黑" class="content"></uv-button>
+                <uv-button type="primary" :plain="true" text="纯净蓝" class="content"></uv-button>
+                <uv-button type="primary" :plain="true" text="中国红" class="content"></uv-button>
+              </view>
+              <view class="content-2">
+                <uv-button type="primary" :plain="true" text="象牙白/浮云黑"></uv-button>
+                <uv-button type="primary" :plain="true" text="象牙白/复古绿" class="content"></uv-button>
+              </view>
             </view>
           </view>
+          <!-- 尺码 -->
+          <view style="margin-top: 20px;">尺码</view>
+          <!-- 尺码结束 -->
+          <view class="content-button1">
+            <view class=" content-button">
+              <view class="content-2">
+                <uv-button type="primary" :plain="true" text="36"></uv-button>
+                <uv-button type="primary" :plain="true" text="37" class="content"></uv-button>
+                <uv-button type="primary" :plain="true" text="38" class="content"></uv-button>
+                <uv-button type="primary" :plain="true" text="39" class="content"></uv-button>
+                <uv-button type="primary" :plain="true" text="40" class="content"></uv-button>
+                <uv-button type="primary" :plain="true" text="41" class="content"></uv-button>
+              </view>
+              <view class="content-2">
+                <uv-button type="primary" :plain="true" text="42"></uv-button>
+                <uv-button type="primary" :plain="true" text="43" class="content"></uv-button>
+                <uv-button type="primary" :plain="true" text="44" class="content"></uv-button>
+              </view>
+            </view>
+          </view>
+          <!-- 号码 -->
+          <!-- 数量 -->
+          <view style="margin-top: 20px;" class="number">
+            <view> 数量</view>
+            <view class="number1">
+              <button>-</button>1
+              <button>+</button>
+            </view>
+          </view>
+          <!-- 数量结束 -->
         </view>
+        <!-- djsid  -->
+         <view class="queding">确定</view>
       </view>
     </uv-popup>
 
@@ -64,23 +87,58 @@ export default {
 .box {
 
   width: 100%;
+  .queding{
+    text-align: center;
+    line-height: 40px;
+    height: 40px;
+    margin-top: 20px;
+    border-radius: 500px;
+    background-color: #C1A766;
+  }
+  // 开始、
+  .number {
+    display: flex;
+    justify-content: space-between;
+   
+    .number1 {
+      width: 30%;
+      height: 30px;
+      align-items: center;
+      display: flex;
+    }
+  }
 
-  // 开始
+  .content-content {
+    width: 100%;
 
+    .content-button1 {
+      width: 100%;
+
+      .content-2 {
+        margin-top: 10px;
+        display: flex;
+
+        .content {
+          margin-left: 25px;
+        }
+      }
+    }
+  }
 
   // 结束
   .content-top {
     display: flex;
     margin-top: 20px;
-    border: 1px solid red;
+
 
     .content-top-image {
-      border: 1px solid navy;
+
       width: 30%;
 
       image {
         width: 100%;
         height: 100px;
+        border-radius: 10px;
       }
     }
 
