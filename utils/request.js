ab491a9064e8584d7b14acb61809eb94dc396af3
mowuchenxiao <643348224@qq.com>
2024-06-19 14:17:00 +0800

    1

--- utils/request.js
+++ utils/request.js
@@ -2,11 +2,9 @@ import { useTokenStore } from "../stores/token"
 import { storToRefs } from "pinia"
 const store = useTokenStore()
 const {token} = storeTorefs(store)
-<<<<<<< HEAD
-const baseURl = 'http://test.wql888.cn/api/'
-=======
+
 const baseURl = 'https://test.wql888.cn/api'
->>>>>>> a3986eb8886cb424861ba0a3b94d01bb54871d86
+
 export default function request({
   url,
   data,
